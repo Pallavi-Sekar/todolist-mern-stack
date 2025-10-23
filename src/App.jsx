22e43aa84171e4cdc5259473cb13aa2bf1db32ad
Pallavi-Sekar <er.pallavisekar@gmail.com>
2025-10-22 23:43:14 -0400

    url updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home.jsx'
+
+function App() {
+
+  return (
+   <div className="create-form">
+    <Home/>
+   </div>
+  )
+}
+
+export default App
